bb3a3985392266905a58668ab28eac21f7b1077a
Luis Gustavo <l.gustavomelo@hotmail.com>
2022-03-11 11:34:18 -0300

    feat: req 2 com mudança

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
 SELECT
-	(SELECT COUNT(song_id) FROM SpotifyClone.Cancoes) cancoes,
-    (SELECT COUNT(DISTINCT artist_id) FROM SpotifyClone.Artista) artistas ,
+	(SELECT COUNT(cancoes_id) FROM SpotifyClone.Cancoes) cancoes,
+    (SELECT COUNT(DISTINCT artista_id) FROM SpotifyClone.Artista) artistas ,
     (SELECT COUNT(DISTINCT album_id) FROM SpotifyClone.Album) albuns;
